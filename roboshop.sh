4f17e8164a0f013cef0069d3ab1a971657ea50e1
venkatesh <venkateshvenky626@gmail.com>
2025-03-27 19:26:55 +0530

    add 2nd script

--- roboshop.sh
+++ roboshop.sh
@@ -4,7 +4,7 @@ AMI=ami-0f3c7d07486cad139 #this keeps on changing
 SG_ID=sg-087e7afb3a936fce7 #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 ZONE_ID=Z104317737D96UJVA7NEF # replace your zone ID
-DOMAIN_NAME="daws76s.online"
+DOMAIN_NAME="venky.shop"
 
 for i in "${INSTANCES[@]}"
 do
